15ab65f664512fcf8acd456d4bd2e669147c55c0
john <johndpowell02@gmail.com>
2022-01-28 13:00:31 -0600

    add various constructors and change from 128 bitset to bitset parts

--- UUID.cpp
+++ UUID.cpp
@@ -4,11 +4,51 @@
  * Constructors
  */
 UUID::UUID()
-    : m_bitset{}, m_version{0} {
+    : m_version{Version::Nil} {
+}
+
+UUID::UUID(Version ver)
+    : m_version{ver} {
+    switch(ver) {
+        case Version::Nil:
+            break;
+        case Version::Version1:
+            break;
+        case Version::Version2:
+            break;
+        case Version::Version3:
+            break;
+        case Version::Version4:
+            break;
+        case Version::Version5:
+            break;
+    }
 }
 
 UUID::UUID(const UUID &uuid)
-    : m_bitset{uuid.m_bitset}, m_version{uuid.m_version} {
+    : m_version{uuid.m_version},
+      m_time_low{uuid.m_time_low}, m_time_mid{uuid.m_time_mid},
+      m_time_hi_and_version{uuid.m_time_hi_and_version},
+      m_clock_seq_hi_and_reserved{uuid.m_clock_seq_hi_and_reserved},
+      m_clock_seq_low{uuid.m_clock_seq_low},
+      m_node{uuid.m_node} {
+}
+
+UUID::UUID(const unsigned long &time_low, const unsigned long &time_mid, const unsigned long &time_hi_and_version,
+           const unsigned long &clock_seq_hi_and_reserved, const unsigned long &clock_seq_low,
+           const unsigned long long &node)
+    : m_time_low{time_low}, m_time_mid{time_mid}, m_time_hi_and_version{time_hi_and_version},
+      m_clock_seq_hi_and_reserved{clock_seq_hi_and_reserved}, m_clock_seq_low{clock_seq_low},
+      m_node{node} {
+    m_version = Version::Nil;
+}
+
+UUID::UUID(const std::string &time_low, const std::string &time_mid, const std::string &time_hi_and_version,
+           const std::string &clock_seq_hi_and_reserved, const std::string &clock_seq_low, const std::string &node)
+   : m_time_low{time_low}, m_time_mid{time_mid}, m_time_hi_and_version{time_hi_and_version},
+     m_clock_seq_hi_and_reserved{clock_seq_hi_and_reserved}, m_clock_seq_low{clock_seq_low},
+     m_node{node} {
+    m_version = Version::Nil;
 }
 
 /*
@@ -18,24 +58,29 @@ unsigned int UUID::get_version() const {
     return m_version;
 }
 
-std::bitset<128> UUID::get_bitset() const {
-    return m_bitset;
-}
-
 /*
  * Functions
  */
-
+//void UUID::parse_version() {
+//    std::bitset<4> ver_set;
+//    for (size_t i{12}; i<16; ++i) {
+//        ver_set[i-12] = m_time_hi_and_version[i];
+//    }
+//}
 std::string UUID::str() {
-    std::string uuid_str{m_bitset.to_string()};
-
     std::stringstream ss;
-    ss << uuid_str.substr(0 , 32) << '-'  // time-low
-       << uuid_str.substr(32, 16) << '-'  // time-mid
-       << uuid_str.substr(48, 16) << '-'  // time-high-and-version
-       << uuid_str.substr(64, 8 )         // clock-seq-and-reserved
-       << uuid_str.substr(72, 8 ) << '-'  // clock-seq-low
-       << uuid_str.substr(80, 48);        // node
+    ss << std::hex << std::setfill('0');
+
+    ss << std::setw(8 ) << m_time_low.to_ulong()            << '-'  // time-low
+       << std::setw(4 ) << m_time_mid.to_ulong()            << '-'  // time-mid
+       << std::setw(4 ) << m_time_hi_and_version.to_ulong() << '-'  // time-high-and-version
+       << std::setw(2 ) << m_clock_seq_hi_and_reserved.to_ulong()   // clock-seq-and-reserved
+       << std::setw(2 ) << m_clock_seq_low.to_ulong()       << '-'  // clock-seq-low
+       << std::setw(12) << m_node.to_ullong();                      // node
 
    return ss.str();
+}
+
+std::string UUID::urn_str() {
+    return "urn:uuid:" + str();
 }
--- UUID.h
+++ UUID.h
@@ -4,24 +4,63 @@
 #include <bitset>
 #include <string>
 #include <sstream>
+#include <iomanip>
+
+#include "VersionBitsets.h"
 
 class UUID {
+public:
+    enum Version {
+        Nil,
+        Version1,
+        Version2,
+        Version3,
+        Version4,
+        Version5
+    };
+
 private:
-    unsigned int m_version;
-    std::bitset<128> m_bitset;
+    Version               m_version;
+
+    const std::bitset<32> m_time_low;
+    const std::bitset<16> m_time_mid;
+    const std::bitset<16> m_time_hi_and_version;
+    const std::bitset<8>  m_clock_seq_hi_and_reserved;
+    const std::bitset<8>  m_clock_seq_low;
+    const std::bitset<48> m_node;
+
+    void parse_version();
 
 public:
     // No-args constructor creates a nil UUID
     UUID();
+    // Version constructor
+    UUID(Version ver);
     // Copy constructor
     UUID(const UUID &uuid);
+    // Integer constructor
+    UUID(const unsigned long& time_low,
+         const unsigned long& time_mid,
+         const unsigned long& time_hi_and_version,
+         const unsigned long& clock_seq_hi_and_reserved,
+         const unsigned long& clock_seq_low,
+         const unsigned long long& node);
+    // String constructor
+    UUID(const std::string& time_low,
+         const std::string& time_mid,
+         const std::string& time_hi_and_version,
+         const std::string& clock_seq_hi_and_reserved,
+         const std::string& clock_seq_low,
+         const std::string& node);
 
     // Getters
     unsigned int get_version() const;
-    std::bitset<128> get_bitset() const;
 
     // Get a string representation of the UUID
     std::string str();
+    // Get a string representation of the UUID as a URN
+    // Looks like urn:uuid:{uuid}
+    std::string urn_str();
 };
 
 
