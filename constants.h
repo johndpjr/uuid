ab0b5b6e2d3eb2a5d9918e5711635cc4b9d286e7
John Powell <john.powell@vetty.co>
2022-02-04 20:27:36 -0600

    initial commit

---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef UUID__CONSTANTS_H
+#define UUID__CONSTANTS_H
+
+namespace constants {
+    // Number of 100ns ticks between October 15, 1582 and January 1, 1970
+    const uint64_t DELTA_100ns_TICKS_GREG_AND_UNIX_EPOCH {0x1B21DD213814000};
+}
+
+#endif //UUID__CONSTANTS_H
