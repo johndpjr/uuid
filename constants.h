a6acc2a632bca87fd4437e3936238d2038ee1753
John Powell <john.powell@vetty.co>
2022-02-13 22:09:44 -0600

    add constant UUIDS_PER_MICROSECOND_TICK to cap how many UUIDs can be generated in one microsecond

--- constants.h
+++ constants.h
@@ -3,7 +3,12 @@
 
 namespace constants {
     // Number of 100ns ticks between October 15, 1582 and January 1, 1970
-    const uint64_t DELTA_100ns_TICKS_GREG_AND_UNIX_EPOCH {0x1B21DD213814000};
+    const uint64_t      DELTA_100ns_TICKS_GREG_AND_UNIX_EPOCH {0x1B21DD213814000};
+    // Number of UUIDs that can be generated every microsecond
+    //     The clock sequence hi and low has 2^13 (8192) distinct values (excluding the variant)
+    //     This means 8192 UUIDs can be generated for the same timestamp
+    //     (effectively meaning 8192 UUIDs can be generated every microsecond)
+    const unsigned long UUIDS_PER_MICROSECOND_TICK            {8'192};
 }
 
 #endif //UUID__CONSTANTS_H
