cb8b46ea9172fbeff1577cf324e2fcb06dcaa712
John Powell <john.powell@vetty.co>
2022-03-03 17:38:06 -0600

    modify UUIDS_PER_MICROSECOND_TICK to MAX_UUIDS_PER_TICK to abstract better and add CLOCK_SEQ_MASK to mask out the 3 MSB of the clock-seq

--- constants.h
+++ constants.h
@@ -3,11 +3,12 @@
 
 namespace constants {
     // Number of 100ns ticks between October 15, 1582 and January 1, 1970
-    const uint64_t DELTA_100ns_TICKS_GREG_AND_UNIX_EPOCH{0x1B21DD213814000};
-    // Number of UUIDs that can be generated every microsecond
-    //     The clock sequence hi and low has 2^13 (8192) distinct values (excluding the variant)
-    //     This means 8192 UUIDs can be generated for the same timestamp
-    //     (effectively meaning 8192 UUIDs can be generated every microsecond)
-    const unsigned long UUIDS_PER_MICROSECOND_TICK{8'192};
+    const uint64_t DELTA_100ns_TICKS_GREG_AND_UNIX_EPOCH {0x1B21DD213814000};
+    // Number of UUIDs that can be generated every tick
+    //  given the same timestamp (and microsecond clock granularity)
+    const unsigned long MAX_UUIDS_PER_TICK {10};
+    // Masks out the 3 most-significant bits of the clock sequence
+    //  to leave room for the variant
+    const unsigned int CLOCK_SEQ_MASK {0x1FFF};
 }
 #endif //UUID__CONSTANTS_H
